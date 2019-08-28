b874e40a7c28d773d7d4a6950618a417d747d5f7
Khanh Nguyen <khnguyen21@ssis.edu.vn>
2019-08-28 13:29:11 +0700

    Create table_reservation.py

---
+++ table_reservation.py
@@ -0,0 +1,6 @@
+nameIn = input('Name:')
+reservation_name = 'Shonda'
+if nameIn == reservation_name:
+    print('Right this way!')
+else:
+    print('Sorry, we dont have a reservation under that name')
